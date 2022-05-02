5314e45089e26c110fbe23df955fc999ce29821d
chaptsand <chaptsand@gmail.com>
2022-05-02 22:08:14 +0100

    cmi: Update blobs from CMI V13.0.3.0.SJBCNXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ TARGET_OTA_ASSERT_DEVICE := cmi
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # Fingerprint
-BUILD_FINGERPRINT := Xiaomi/cmi/cmi:11/RKQ1.200826.002/V12.5.10.0.RJBCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/cmi/cmi:12/SKQ1.211006.001/V13.0.3.0.SJACNXM:user/release-keys
 
 # Kernel
 TARGET_KERNEL_CONFIG := cmi_user_defconfig
