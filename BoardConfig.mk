cba9a6de1eeda0d48152562ba6ef550ee2872e3e
yarpiin <yarpiin@gmail.com>
2022-09-18 21:13:52 +0100

    cmi: drop stock fingerprint

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@ TARGET_OTA_ASSERT_DEVICE := cmi
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Fingerprint
-BUILD_FINGERPRINT := Xiaomi/cmi/cmi:12/RKQ1.211001.001/V13.0.4.0.SJACNXM:user/release-keys
-
 # Kernel
 TARGET_KERNEL_CONFIG := cmi_user_defconfig
 
